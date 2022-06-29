27d03a396f43c485001a976ee97caa4f60ba091b
Noelia Parada <noeliacastrelo@gmail.com>
2022-06-29 22:55:47 +0200

    dark theme and emoji btn

--- noelia-portfolio/src/App.js
+++ noelia-portfolio/src/App.js
@@ -1,10 +1,29 @@
-
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [lightTheme, setLightTheme] = useState(true)
+
+  const darkStyles = {
+    backgroundColor: "black",
+    color: "white"
+  }
+
+  const lightStyles = {
+    backgroundColor: "white",
+    color: "black"
+  }
+
+  const handleToggle= () => {
+   setLightTheme(!lightTheme)
+  }
+  
   return (
-    <div className="App">
-      <h1>Noelia Portfolio</h1>
+    <div className="App" style={lightTheme ? lightStyles:darkStyles }>
+    <button onClick={handleToggle} className="lightThemeBtn">{lightTheme ? "🌚" : "🌝"}</button>
+      <h1>Hey you! Welcome to my Portfolio!</h1>
     </div>
   );
 }
