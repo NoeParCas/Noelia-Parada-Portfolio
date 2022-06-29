b2a98db7979d668a882795d4ab01336525fd30a3
Noelia Parada <noeliacastrelo@gmail.com>
2022-06-29 23:17:49 +0200

    Moncho the pug added

--- noelia-portfolio/src/App.js
+++ noelia-portfolio/src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import './App.css';
-
+import { Link } from "react-router-dom";
 
 function App() {
 
   const [lightTheme, setLightTheme] = useState(true)
-
+  
   const darkStyles = {
     backgroundColor: "black",
     color: "white"
@@ -19,11 +19,15 @@ function App() {
   const handleToggle= () => {
    setLightTheme(!lightTheme)
   }
-  
+
   return (
     <div className="App" style={lightTheme ? lightStyles:darkStyles }>
     <button onClick={handleToggle} className="lightThemeBtn">{lightTheme ? "🌚" : "🌝"}</button>
       <h1>Hey you! Welcome to my Portfolio!</h1>
+      <h2>I am Noelia Parada Castro, a Junior Software Developer, dog lover and gastronomy enthusiast.</h2>
+      <h4>Feel free to look around, Moncho the Pug will guide you!</h4>
+      <Link to="#"><img display="flex" src="https://i.pinimg.com/originals/f8/97/e2/f897e2e871ed90d572f23b0539397432.gif" width="130" height="100" /></Link>
+
     </div>
   );
 }
